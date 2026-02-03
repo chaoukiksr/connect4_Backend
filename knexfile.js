8c02460f8d87ef95fa402afd73f9e5f104864073
chaoukiksr <107209548+chaoukiksr@users.noreply.github.com>
2026-02-04 00:50:55 +0100

    db deploy setup

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,11 @@ module.exports = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      port: process.env.DB_PORT
+      port: process.env.DB_PORT,
+      ssl: {
+         minVersion: 'TLSv1.2',
+         rejectUnauthorized: true
+      }
    },
    migrations:{
       directory:'./migrations'
